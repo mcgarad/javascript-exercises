e6f890da93083184b860691a7e15fcb44a95ee72
mcgarad <mcgarad@gmail.com>
2022-02-16 23:09:38 -0500

    complete exercises 01-03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(message, repetitions) {
+    let repeatedString = '';
+    if (repetitions < 0) {
+        repeatedString = 'ERROR';
+    } else {
+        for (i = 1; i <= repetitions; i++) {
+            repeatedString += message;
+        }
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(message) {
+    let messageArray = message.split('').reverse();
+    let reversedMessage = '';
+    for (i = 0; i < messageArray.length; i++) {
+        reversedMessage += messageArray[i];
+    }
+    return reversedMessage;
 };
 
 // Do not edit below this line
