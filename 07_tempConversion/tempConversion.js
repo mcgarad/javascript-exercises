80a26ad642190ad5e6c3caffb11972c9e85b8001
mcgarad <mcgarad@gmail.com>
2022-02-17 15:31:50 -0500

    complete exercises 06 & 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  let cel = (far - 32) * 5 / 9;
+  return Math.round(cel * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let far = cel * 9 / 5 + 32;
+  return Math.round(far * 10) / 10;
 };
 
 // Do not edit below this line
