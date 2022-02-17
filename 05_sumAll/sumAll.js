5f535962421b0ba50aa2c90899ef76f59828cd72
mcgarad <mcgarad@gmail.com>
2022-02-17 15:10:29 -0500

    complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
+    let lowNum = 0;
+    let highNum = 0;
+    if (validateInput(startNum) && validateInput(endNum)) {
+        if (startNum < endNum) {
+            lowNum = startNum;
+            highNum = endNum;
+        } else {
+            lowNum = endNum;
+            highNum = startNum;
+        }
+        for (let i = lowNum; i <= highNum; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {return 'ERROR'}
+};
 
+const validateInput = function(checkedInput) {
+    let valid = true;
+    if (Number.isInteger(checkedInput) === false || checkedInput < 0) {
+        valid = false;
+    }
+    return valid;
 };
 
 // Do not edit below this line
