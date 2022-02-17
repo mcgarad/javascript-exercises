acb7c36a9945d2416a224cdb85197c7edd7817f7
mcgarad <mcgarad@gmail.com>
2022-02-17 13:49:42 -0500

    complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list, ...removedValues) {
+    let filteredList = []
+    for (i = 0; i < list.length; i++){
+        if (removedValues.includes(list[i])) {
+            continue;
+        } else {filteredList.push(list[i])}
+    }
+    return filteredList;
 };
 
 // Do not edit below this line
